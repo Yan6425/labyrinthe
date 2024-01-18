6290d920ae4adaea399258a9a79a0387094bb5f0
Maël Lescoulié <lescouliem@cy-tech.fr>
2024-01-18 09:40:19 +0100

    Ajout de nouveaux commentaires

--- fonctions.h
+++ fonctions.h
@@ -43,6 +43,7 @@ Joueur avionPlus(Joueur j, int shield);
 /* Enlève l'avion au joueur */
 Joueur avionMoins(Joueur j, int shield);
 
+/* Fonction qui gère l'éta de fatigue du joueur quand il a pris un somnifere */
 void somnifere(Joueur j,int* som);
 
 /* Vérifie si le joueur est mort */
@@ -66,18 +67,22 @@ int deplacement(int** labyrinthe,int n,Joueur j,int hauteur, int largeur,int* fi
 /* Affiche le labyrinthe */
 void afficherLabyrinthe(int** labyrinthe,int a, int b, Joueur u);
 
+/* Affiche le labyrinthe en entier quand on prend l'objet carte */
 void afficherMap(int** labyrinthe,int hauteur, int largeur);
 
 /* Affiche l'énergie restante du joueur */
 void afficherVie(Joueur j);
 
+/* Affiche la barre de sommeil après avoir pris l'objet somnifere */
 void afficherSomnifere(Joueur j,int n);
 
 /* Affiche le niveau actuel du jeu */
 void afficherNiveau(int n);
 
+/* Affiche le message de défaite en fonction de la manière dont le joueur a perdu */
 void afficherMort(int n,int** labyrinthe, int hauteur, int largeur, Joueur j,int som);
 
+/* Affiche le message de victoire à la fin de chaque labyrinthe */
 void afficherVictoire(int n,int** labyrinthe, int hauteur, int largeur, Joueur j);
 
 // plateau.c
@@ -96,6 +101,7 @@ void initialiserLabyrinthe(int** labyrinthe, int n, int N);
 
 /* Lis le fichier .txt pour le transférer dans la matrice */
 void lireTXT(const char* nomFichier, int** labyrinthe);
+
 void creerArbre(Arbre arbre, int** labyrinthe, int x, int y, int hauteur, int largeur);
 void initDejaVus(int** dejaVus, int hauteur, int largeur);
 Arbre trouverNoeud(Arbre arbre, int x, int y, int** dejaVus, int hauteur, int largeur);
